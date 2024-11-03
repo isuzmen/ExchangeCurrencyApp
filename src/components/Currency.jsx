cc196bc821cbac2d886cc74bf3cae71a15a720f8
İlyas Süzmen <ilyas.szmn@gmail.com>
2024-11-03 18:12:10 +0300

    Add currency exchange API integration with axios

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/currency.css';
+import axios from 'axios';
+
+const BASE_URL = "https://api.freecurrencyapi.com/v1/latest";
+const API_KEY = "fca_live_J36yAbW48ecUElIa5gsArAwndeECDTJSFwFSrx0m";
 
 function Currency() {
+  const [amount, setAmount] = useState('');
+  const [fromCurrency, setFromCurrency] = useState('USD');
+  const [toCurrency, setToCurrency] = useState('TRY');
+  const [result, setResult] = useState('');
+
+  const exchange = async () => {
+    const response = await axios.get(`${BASE_URL}?apikey=${API_KEY}&base_currency=${fromCurrency}`);
+    const calculatedResult = (response.data.data[toCurrency] * amount).toFixed(2);
+    setResult(calculatedResult);
+  };
+
   return (
     <div className='currency-div'>
       <div style={{ marginTop: '-20px', color: 'white', fontFamily: 'arial', backgroundColor: 'black', width: '100%', textAlign: 'center' }}>
@@ -9,9 +24,10 @@ function Currency() {
       </div>
       <div style={{ marginTop: '25px' }}>
         <input
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
           type="number"
-          className="amount"
-          placeholder="Amount"
+          className='amount'
         />
       </div>
     </div>
@@ -19,3 +35,4 @@ function Currency() {
 }
 
 export default Currency;
+
