2c0de624b1042e9f026ada1ae071745abfe0d1b0
İlyas Süzmen <ilyas.szmn@gmail.com>
2024-11-03 17:42:17 +0300

    Add structure for Currency component

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -4,7 +4,16 @@ import '../css/currency.css';
 function Currency() {
   return (
     <div className='currency-div'>
-      
+      <div style={{ marginTop: '-20px', color: 'white', fontFamily: 'arial', backgroundColor: 'black', width: '100%', textAlign: 'center' }}>
+        <h3>CURRENCY EXCHANGE APP</h3>
+      </div>
+      <div style={{ marginTop: '25px' }}>
+        <input
+          type="number"
+          className="amount"
+          placeholder="Amount"
+        />
+      </div>
     </div>
   );
 }
