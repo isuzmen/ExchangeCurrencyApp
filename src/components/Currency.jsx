d82d556e7faf9251da8b55f6ad1cb92d325426e3
İlyas Süzmen <ilyas.szmn@gmail.com>
2024-11-03 17:34:34 +0300

    Initialize project structure with App and Currency components

---
+++ src/components/Currency.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/currency.css';
+
+function Currency() {
+  return (
+    <div className='currency-div'>
+      
+    </div>
+  );
+}
+
+export default Currency;
